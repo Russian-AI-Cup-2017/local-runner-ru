7dcfb15760ed3cfbf608b92c54165ad4449a1e22
Alex Shumsky <alexthreed@gmail.com>
2017-11-13 12:25:45 +0300

    Forward *.sh command line parameters and chmod +x *.sh

--- local-runner-console.sh
+++ local-runner-console.sh
@@ -1,1 +1,1 @@
-java -Xms512m -Xmx1G -server -jar "local-runner.jar" local-runner-console.properties local-runner-console.default.properties
+java -Xms512m -Xmx1G -server -jar "local-runner.jar" local-runner-console.properties local-runner-console.default.properties "$@"
